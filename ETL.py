ae2b0fca1cea398a7c79526e3a4310424bbb4a96
Panus <panus2001@gmail.com>
2023-04-28 19:31:48 +0700

    End of 28 Apr

--- ETL.py
+++ ETL.py
@@ -2,32 +2,21 @@ import mysql.connector
 import schedule
 import time
 import pandas as pd
+from json import dumps
+from kafka import KafkaProducer
 
 # Have not cap create_date yet -> add WHERE creat_at > DATE(NOW()) - 1
 
 listOfPatient = tuple(range(1, 501, 1))
 
 patientSql = """
-SELECT patient_hn, t_patient_id, CONCAT(patient_firstname, ' ', patient_lastname), patient_birthday, f_sex_id, patient_patient_mobile_phone 
+SELECT patient_hn, t_patient_id, CONCAT(patient_firstname, ' ', patient_lastname), patient_birthday, f_sex_id, patient_patient_mobile_phone,
+CONCAT(patient_contact_firstname, ' ', patient_contact_lastname), "Emergency contact person", patient_contact_sex_id, patient_contact_phone_number
 FROM t_patient
 WHERE patient_hn IN
 """ + str(listOfPatient)
 
-patientColumns = ['uid', 'cid', 'name', 'birthDate', 'gender', 'telecom']
-
-contactSql = """
-SELECT patient_hn, CONCAT(patient_contact_firstname, ' ', patient_contact_lastname), "Emergency contact person", patient_contact_sex_id, patient_contact_phone_number
-FROM t_patient
-WHERE patient_hn IN
-""" + str(listOfPatient)
-
-contactColumns = ['uid', 'name', 'relationship', 'gender', 'telcom']
-
-medicationSql = """
-SELECT b_item_drug_id, item_drug_printable FROM b_item_drug
-"""
-
-medicationColumns = ['code', 'display']
+patientColumns = ['uid', 'cid', 'name', 'birthDate', 'gender', 'telecom', 'contact_name', 'contact_relationship', 'contact_gender', 'contact_telcom']
 
 isTakingSql = """
 SELECT v.visit_hn, b_item_id, o.order_date_time, od.order_drug_printable, od.order_drug_special_prescription_text
@@ -37,8 +26,6 @@ WHERE v.t_visit_id = o.t_visit_id AND o.t_order_id = od.t_order_id AND visit_hn 
 
 isTakingColumns = ['uid', 'code', 'authoredOn', 'dosageInstruction', 'note']
 
-# AllergicIntorelanceSubstanceSql is the same as medication -> omit this part
-
 isAllergicSql = """
 SELECT p.patient_hn, b_item_id, 'active', 'confirmed', 'allergy', 'medication', patient_drug_allergy_symptom, patient_drug_allergy_record_date_time
 FROM t_patient_drug_allergy a, t_patient p
@@ -46,12 +33,6 @@ WHERE a.t_patient_id = p.t_patient_id AND patient_hn IN
 """ + str(listOfPatient)
 
 isAllergicColumns = ['uid', 'code', 'clinicalStatus', 'verificationStatus', 'type', 'category', 'criticality', 'recordDate']
-
-conditionSql = """
-SELECT icd10_number, icd10_description FROM b_icd10
-"""
-
-conditionColumns = ['code', 'display']
 
 isHavingSql = """
 SELECT v.visit_hn, diag_icd10_number, CASE WHEN diag_icd10_active = 1 THEN 'active' ELSE 'resolved' END as active,
@@ -76,43 +57,50 @@ def transform(sql, columns):
     mycursor.execute(sql)
     result = mycursor.fetchall()
     df = pd.DataFrame.from_records(result, columns=columns)
+    for column in df.columns:
+        try: df[column] = df[column].apply(lambda x: pd.to_datetime(x).isoformat(timespec='milliseconds'))
+        except: continue
     if 'gender' in columns: df.set_index('uid', inplace=True)
     # else: df.set_index('code', inplace=True)
     return df
 
-def job():
+def ETL():
     patient = transform(patientSql, patientColumns).to_dict('index')
-    contact = transform(contactSql, contactColumns).to_dict('index')
-    for i in patient: 
-        contact[i]['uid'] = i
+    # contact = transform(contactSql, contactColumns).to_dict('index')
+    all_patient = dict()
+    for i in patient:
         patient[i]['uid'] = i
-        patient[i]['contact'] = contact[i]
-        patient[i]['isTaking'] = []
-        patient[i]['isAllergic'] = []
-        patient[i]['isHaving'] = []
+        all_patient[i] = dict()
+        all_patient[i]['patient'] = patient[i]
+        all_patient[i]['isTaking'] = []
+        all_patient[i]['isAllergic'] = []
+        all_patient[i]['isHaving'] = []
 
     isTaking = transform(isTakingSql, isTakingColumns).to_dict('index')
     for i in isTaking:
         uid = isTaking[i]['uid']
-        patient[uid]['isTaking'].append(isTaking[i])
+        all_patient[uid]['isTaking'].append(isTaking[i])
 
     isAllergic = transform(isAllergicSql, isAllergicColumns).to_dict('index')
     for i in isAllergic:
         uid = isAllergic[i]['uid']
-        patient[uid]['isAllergic'].append(isAllergic[i])
+        all_patient[uid]['isAllergic'].append(isAllergic[i])
 
     isHaving = transform(isHavingSql, isHavingColumns).to_dict('index')
     for i in isHaving:
         uid = isHaving[i]['uid']
-        patient[uid]['isHaving'].append(isHaving[i])
+        all_patient[uid]['isHaving'].append(isHaving[i])
 
-    print(patient)
-    return
+    list_all_patient = [all_patient[i] for i in all_patient]
+    return str(list_all_patient)
 
-schedule.every(10).seconds.do(job)
-while True:
+def job():
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda x: 
+                         dumps(x).encode('utf-8'))
+    producer.send('numtest', value=ETL())
+
+schedule.every(5).seconds.do(job)
+for i in range(3):
     schedule.run_pending()
-    time.sleep(1)
-
-# print(transform(medicationSql, medicationColumns))
-# print(transform(conditionSql, conditionColumns))
+    time.sleep(5)
--- prepare_db/import_db.py
+++ prepare_db/import_db.py
@@ -1,5 +1,7 @@
 import mysql.connector
 import pandas as pd
+import hashlib
+
 
 def import_to_db(sql, val):
     mydb = mysql.connector.connect(
@@ -15,32 +17,34 @@ def import_to_db(sql, val):
 
 def t_patient():
     sql = """
-    INSERT INTO t_patient (t_patient_id, patient_firstname, patient_lastname,
+    INSERT INTO t_patient (patient_hn, t_patient_id, patient_firstname, patient_lastname,
         patient_birthday, f_sex_id, patient_patient_mobile_phone,
         patient_contact_firstname, patient_contact_lastname,
         patient_contact_sex_id, patient_contact_phone_number) 
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     """
-    df = pd.read_excel('HosOS_mock_data.xlsx', sheet_name='t_patient', dtype=str)
+    df = pd.read_excel('.\prepare_db\HosOS_mock_data.xlsx', sheet_name='t_patient', dtype=str)
     df['patient_birthday'] = df['patient_birthday'].apply(lambda x: x[:10])
-    val = df[df.columns[df.columns != 'patient_hn']].to_records(index=False)
-    return sql, val
+    df['patient_hn'] = df['t_patient_id'].apply(lambda x: hashlib.sha256(x.encode()).hexdigest())
+    val = df.to_records(index=False)
+    return sql, val, df[['patient_hn']].to_dict()['patient_hn']
 
 def b_item_drug():
     sql = """
     INSERT INTO b_item_drug (b_item_drug_id, item_drug_printable)
     VALUES (%s, %s)
     """
-    df = pd.read_excel('HosOS_mock_data.xlsx', sheet_name='b_item_drug', dtype=str)
+    df = pd.read_excel('.\prepare_db\HosOS_mock_data.xlsx', sheet_name='b_item_drug', dtype=str)
     val = df.to_records(index=False)
     return sql, val
 
-def t_visit():
+def t_visit(hn):
     sql = """
     INSERT INTO t_visit (visit_hn, visit_date)
     VALUES (%s, %s)
     """
-    df = pd.read_excel('HosOS_mock_data.xlsx', sheet_name='t_visit', dtype=str)
+    df = pd.read_excel('.\prepare_db\HosOS_mock_data.xlsx', sheet_name='t_visit', dtype=str)
+    df['visit_hn'] = df['visit_hn'].apply(lambda x: hn[int(x) - 1])
     val = df[df.columns[df.columns != 't_visit_id']].to_records(index=False)
     return sql, val
 
@@ -49,7 +53,7 @@ def t_order():
     INSERT INTO t_order (t_visit_id, b_item_id, order_date_time)
     VALUES (%s, %s, %s)
     """
-    df = pd.read_excel('HosOS_mock_data.xlsx', sheet_name='t_order', dtype=str)
+    df = pd.read_excel('.\prepare_db\HosOS_mock_data.xlsx', sheet_name='t_order', dtype=str)
     val = df[df.columns[df.columns != 't_order_id']].to_records(index=False)
     return sql, val
 
@@ -58,16 +62,25 @@ def t_order_drug():
     INSERT INTO t_order_drug (t_order_id, order_drug_printable, order_drug_special_prescription_text, order_drug_date_time)
     VALUES (%s, %s, %s, %s)
     """
-    df = pd.read_excel('HosOS_mock_data.xlsx', sheet_name='t_order_drug', dtype=str)
+    df = pd.read_excel('.\prepare_db\HosOS_mock_data.xlsx', sheet_name='t_order_drug', dtype=str)
     val = df[df.columns[df.columns != 't_order_drug_id']].to_records(index=False)
     return sql, val   
+
+def b_allergy_agents():
+    sql = """
+    INSERT INTO b_allergy_agents (b_allergy_item_id, item_allergy_printable)
+    VALUES (%s, %s)
+    """
+    df = pd.read_excel('.\prepare_db\HosOS_mock_data.xlsx', sheet_name='b_allergy_agents', dtype=str)
+    val = df.to_records(index=False)
+    return sql, val
 
 def t_patient_drug_allergy():
     sql = """
     INSERT INTO t_patient_drug_allergy (t_patient_id, b_item_id, patient_drug_allergy_symptom, patient_drug_allergy_record_date_time)
     VALUES (%s, %s, %s, %s)
     """
-    df = pd.read_excel('HosOS_mock_data.xlsx', sheet_name='t_patient_drug_allergy', dtype=str)
+    df = pd.read_excel('.\prepare_db\HosOS_mock_data.xlsx', sheet_name='t_patient_drug_allergy', dtype=str)
     val = df.to_records(index=False)
     return sql, val
 
@@ -76,7 +89,7 @@ def b_icd10():
     INSERT INTO b_icd10 (icd10_number, icd10_description)
     VALUES (%s, %s)
     """
-    df = pd.read_excel('HosOS_mock_data.xlsx', sheet_name='b_icd10', dtype=str)
+    df = pd.read_excel('.\prepare_db\HosOS_mock_data.xlsx', sheet_name='b_icd10', dtype=str)
     val = df.to_records(index=False)
     return sql, val
 
@@ -86,23 +99,26 @@ def t_diag_icd10():
     diag_icd10_notice, diag_icd10_record_date_time, diag_icd10_active)
     VALUES (%s, %s, %s, %s, %s)
     """
-    df = pd.read_excel('HosOS_mock_data.xlsx', sheet_name='t_diag_icd10', dtype=str)
+    df = pd.read_excel('.\prepare_db\HosOS_mock_data.xlsx', sheet_name='t_diag_icd10', dtype=str)
     val = df[df.columns[df.columns != 't_diag_icd10_id']].to_records(index=False)
     return sql, val
 
-sql, val = t_patient()
+sql, val, hn = t_patient()
 import_to_db(sql, val)
 
 sql, val = b_item_drug()
 import_to_db(sql, val)
 
-sql, val = t_visit()
+sql, val = t_visit(hn)
 import_to_db(sql, val)
 
 sql, val = t_order()
 import_to_db(sql, val)
 
 sql, val = t_order_drug()
+import_to_db(sql, val)
+
+sql, val = b_allergy_agents()
 import_to_db(sql, val)
 
 sql, val = t_patient_drug_allergy()
